00d5a9996c5daeaadd9faba753ae37f479ea0784
User4 <titvjl9009@mail.ru>
2022-06-10 09:18:40 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,4 @@
+﻿int[] numer = { 9, 3, 4, 5, 6, 7, -8, -6, -7, -3, -5, 0, 3, 0, 0 };
+Console.WriteLine("отрицательные: " + numer.Count(a => a > 0));
+Console.WriteLine("Произведение:" + numer.Where(i => i > 0).Aggregate((x, y) => x * y));
+Console.WriteLine("нулевые:" + numer.Count(a => a == 0));
